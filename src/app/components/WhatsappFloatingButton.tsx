4d501c877d9404de66584cf144c9117ccc89ecb5
André Rodrigues <andre.rodrigues@catenda.no>
2024-12-18 22:44:58 +0000

    Position fixed

--- src/app/components/WhatsappFloatingButton.tsx
+++ src/app/components/WhatsappFloatingButton.tsx
@@ -3,7 +3,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export const WhatsappFloatingbutton = () => {
   const sampleText = 'Olá, gostaria de marcar uma consulta';
   return (
-  <a className="w-14 h-14 absolute bottom bg-[#25d366] rounded-full flex cursor-pointer right-[64px] bottom-[64px] sm:right-[64px] sm:bottom-[64px]" href={`https://wa.me/351919113188?text=${sampleText}`} target="_blank">
+  <a className="w-14 h-14 fixed bottom bg-[#25d366] rounded-full flex cursor-pointer right-[64px] bottom-[64px] sm:right-[64px] sm:bottom-[64px]" href={`https://wa.me/351919113188?text=${sampleText}`} target="_blank">
     <FaWhatsapp className="w-14 h-14 p-2"/>
     </a>
     )
