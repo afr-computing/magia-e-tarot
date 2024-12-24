101e0e615cf71ab7f010d0befe26c7f945bb72f7
André Rodrigues <andre.rodrigues@catenda.no>
2024-12-24 15:33:39 +0000

    Remove empty onClick

--- src/app/components/WhatsappFloatingButton.tsx
+++ src/app/components/WhatsappFloatingButton.tsx
@@ -1,12 +1,9 @@
 import { FaWhatsapp } from "react-icons/fa";
 
 export const WhatsappFloatingbutton = () => {
-  const onClick = () => {
-  };
-
   const sampleText = 'Olá, gostaria de marcar uma consulta';
   return (
-  <a className="w-14 h-14 fixed bottom bg-[#25d366] rounded-full flex cursor-pointer right-[16px] bottom-[16px]" href={`https://wa.me/351919113188?text=${sampleText}`} target="_blank" onClick={onClick}>
+  <a className="w-14 h-14 fixed bottom bg-[#25d366] rounded-full flex cursor-pointer right-[16px] bottom-[16px]" href={`https://wa.me/351919113188?text=${sampleText}`} target="_blank" >
     <FaWhatsapp className="w-14 h-14 p-2"/>
     </a>
     )
