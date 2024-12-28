dde268d93b8afbb7dc5d114f97309218c6f1f001
André Rodrigues <andre.rodrigues@catenda.no>
2024-12-28 20:58:40 +0000

    Add missing link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
           </div>
           <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base/7 font-semibold text-white lg:grid-cols-2 md:flex lg:gap-x-10">
-              <a href="#">Testemunhos <span aria-hidden="true">&rarr;</span></a>
+              <a href="#testemunhos">Testemunhos <span aria-hidden="true">&rarr;</span></a>
               <a href="#">Sobre <span aria-hidden="true">&rarr;</span></a>
               <a href="#">Trabalhos <span aria-hidden="true">&rarr;</span></a>
               <a href="instagram://user?username=loja_luzdaalma" onClick={() => window.open('https://www.instagram.com/loja_luzdaalma/', '_blank')}>Loja luz da alma <span aria-hidden="true">&rarr;</span></a>
