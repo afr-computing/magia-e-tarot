158fa3fcbd05bf7acc4b312d1d4a5e3dfbb4bd3e
André Rodrigues <andre.rodrigues@catenda.no>
2025-01-18 17:38:07 +0000

    Adjust padding

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -28,7 +28,7 @@ export const About = () => {
   return (
     <div id="about" className="relative isolate min-h-[100vh] overflow-hidden bg-gray-900">
       <Image src={AboutPic} alt="about" className="absolute inset-0 -z-10 size-full object-[-80px] object-cover lg:object-top opacity-20" />
-      <div className="md:text-center text-pretty leading-relaxed px-6 md:px-8 py-64 flex items-center gap-6 flex-col md:flex-row flex-wrap">
+      <div className="md:text-center text-pretty leading-relaxed px-6 md:px-8 py-32 flex items-center gap-6 flex-col md:flex-row flex-wrap">
         <div className="flex-1">
           <h1 className="text-3xl md:text-5xl font-bold text-center mb-12">
             Tarólogo vidente pai José de Ogum
