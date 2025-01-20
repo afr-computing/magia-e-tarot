58b6b5ec36c901710fe4faf669db5d254a62bd98
André Rodrigues <andre.rodrigues@catenda.no>
2025-01-20 18:59:50 +0000

    Add domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/magia-e-tarot",
+  basePath: "/magia-e-tarot.com",
   images: {
     unoptimized: true,
   },
